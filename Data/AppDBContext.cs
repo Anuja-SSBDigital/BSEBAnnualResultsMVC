3180fa405ed80525b256c53451ce407d95f0e5aa
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2026-03-19 15:15:47 +0530

    Add model class in  the cs file

--- Data/AppDBContext.cs
+++ Data/AppDBContext.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BSEBAnnualResultsMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options)
-    {
-    }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){ }
+
+    public DbSet<ExamFinalPublishedResult> FinalPublishedResults { get; set; }
 
     // Tables
-   
+
 }
